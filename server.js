5e9fc51189ee62375c492a91f267bda416286c9a
Yan Naing Win <ynwin@mit.com.mm>
2026-01-16 14:23:41 +0630

    update_yaml

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 // Serve static files from 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
